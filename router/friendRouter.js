e23933feba1d39589b46028433c180cc92c6b0b1
HarnoLee <harnolee@gmail.com>
2018-10-11 15:39:50 +0800

    add attention to sb.

--- router/friendRouter.js
+++ router/friendRouter.js
@@ -1,17 +1,17 @@
 const friendService = require("../service/friendService");
 const router = require("express").Router();
 
-router.put("/", async (req, res) => {
-    const action = req.body.friendAction;
+router.post("/", async (req, res) => {
+    const action = req.body.action;
     let result;
     switch (action) {
-        case 0:
+        case 1:
             await friendService.updateFriend(req.user._id, req.body.attentionId);
             result = "关注好友成功！";
             break;
-        case 1:
+        case 0:
             await friendService.removeFriend(req.user._id, req.body.attentionId);
-            result = "关注好友成功！";
+            result = "取消关注好友成功！";
             break;
         default:
             // res.failure("错误操作！");
--- router/indexRouter.js
+++ router/indexRouter.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const blogService = require("../service/blogService");
 const userDetailService = require("../service/userDetailService");
+const friendService = require("../service/friendService");
 
 router.get("/", async (req, res) => {
     /*
@@ -9,10 +10,16 @@ router.get("/", async (req, res) => {
      */
     const blogs = await blogService.getIndexBlogsByPage();
     const userDetails = await userDetailService.getUserDetailsByPage();
+    // let friends;
+    // if (req.user._id) {  //这里缺少user
+    //     friends = await friendService.getFriendById(req.user._id);
+    // }
     const result = {
         blogs: blogs,
         userDetails: userDetails
     };
+    // if (friends)
+    //     result.friends = friends;
     res.success(result);
 });
 
